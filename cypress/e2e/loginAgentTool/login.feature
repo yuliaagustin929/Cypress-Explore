b97f9daf406f627b22820f2d34aab57ded3258fb
yuliaagustinbfi <yulia.agustin@bfi.co.id>
2024-04-04 14:10:00 +0700

    remove feature

--- cypress/e2e/loginAgentTool/login.feature
+++ cypress/e2e/loginAgentTool/login.feature
@@ -1,33 +1,20 @@
-# @ProjectKey-BAAT
 Feature: Login feature
 
     Background:
         Given Agent access agent tool 'home' page
 
-    # @BAAT-T2377
     Scenario Outline: [Login] Successfully logged in with active account 
-        When Agent login with 'active' credentials
+        When Agent login with '866433277757' credentials
         Then Agent should be able to see lounge page
 
-    # @BAAT-T2377
-    Scenario Outline: [Login] Agent failed to login with inactive account
-        When Agent login with 'inactive' credentials
-        Then Agent can see reactivasi account page
-
-    # @BAAT-T2377
-    Scenario Outline: [Login] Agent failed to login with unregistered account
-        When Agent login with 'unregistered' credentials
-        Then Agent can see pop up message for unregistered agent
-
-    # @BAAT-T2377
     Scenario Outline: Login Validation
         When Agent login with "<phoneNumber>" phone number
         Then Agent can see "<message>" must input valid phone number
 
         Examples:
         | phoneNumber       | message                           |
-        | empty             | Masukkan Nomor Telepon            |
-        | moreThanMaxDigit  | Maksimal 14 digit                 |
-        | lessThanMinDigit  | Minimal 10 digit                  |
-        | invalidFormat     | Masukkan Nomor Ponsel yang valid  |
+        |                   | Masukkan Nomor Telepon            |
+        | 89854656576876    | Maksimal 14 digit                 |
+        | 89656             | Minimal 10 digit                  |
+        | 79656764567       | Masukkan Nomor Ponsel yang valid  |
 
--- cypress/e2e/loginAgentTool/login.js
+++ cypress/e2e/loginAgentTool/login.js
@@ -1,7 +1,6 @@
 /// <reference types="Cypress" />
 
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import loginPage from '../../pom/loginPage'; 
 import { loginAgentUI } from '../commonAction';
 
 Given(`Agent access agent tool {string} page`, (pageName) => {
@@ -12,12 +11,12 @@ Given(`Agent access agent tool {string} page`, (pageName) => {
   }).as('homePage');
   if (pageName == 'home') {
     cy.visit({
-      url: `${Cypress.env('agency-agenttools').baseUrl}`,
+      url: "https://orientasi.sit.bravo.bfi.co.id",
       failOnStatusCode: false
     });
   } else {
     cy.visit({
-      url: `${Cypress.env('agency-agenttools').baseUrl}/${pageName}`,
+      url: `https://orientasi.sit.bravo.bfi.co.id/${pageName}`,
       failOnStatusCode: false
     });
   }
@@ -25,70 +24,41 @@ Given(`Agent access agent tool {string} page`, (pageName) => {
 
 When(`Agent login with {string} credentials`, (phoneNumber) => {
   cy.wait('@homePage');
-  loginPage.loginButton().click();
-  if (phoneNumber === 'active') {
-    loginPage.phoneNumberField().clear().type(Cypress.env('agency-agenttools').phoneActiveBravo);
-  } else if (phoneNumber === 'inactive') {
-    loginPage.phoneNumberField().clear().type(Cypress.env('agency-agenttools').phoneInactive);
-  } else if (phoneNumber === 'unregistered') {
-    loginPage
-      .phoneNumberField()
-      .clear()
-      .type('8305' + Math.floor(Math.random() * 10000000));
-  }
-  loginPage.sendVerifiCodeButton().click();
-  loginPage.inputOtp1().type(1);
-  loginPage.inputOtp2().type(1);
-  loginPage.inputOtp3().type(1);
-  loginPage.inputOtp4().type(1);
-  loginPage.sendVerifiConfirmButton().click();
+  cy.get('#loginButton', { timeout: 17000 }).click();
+  cy.get('[data-testid="mobile_phone"]').clear().type(phoneNumber);
+  cy.get('#sendLoginCodeButton').click();
+  cy.get('[data-testid="otp1"]').type(1);
+  cy.get('[data-testid="otp2"]').type(1);
+  cy.get('[data-testid="otp3"]').type(1);
+  cy.get('[data-testid="otp4"]').type(1);
+  cy.get('#verificationConfirmButton').click();
   cy.intercept('GET', '**/onboarding/v2/agent/profile').as('loungePage');
 });
 
 When(`Agent login with {string} phone number`, (phoneNumber) => {
   cy.wait('@homePage');
-  loginPage.loginButton().click();
-  if (phoneNumber === 'empty') {
-    loginPage.phoneNumberField().type('0').clear();
-  } else if (phoneNumber === 'moreThanMaxDigit') {
-    loginPage.phoneNumberField().clear().type('89854656576876');
-  } else if (phoneNumber === 'lessThanMinDigit') {
-    loginPage.phoneNumberField().clear().type('89656');
-  } else if (phoneNumber === 'invalidFormat') {
-    loginPage.phoneNumberField().clear().type('79656764567');
-  }
+  cy.get('#loginButton', { timeout: 17000 }).click();
+    if (phoneNumber === '') {
+      cy.get('[data-testid="mobile_phone"]').type(0).clear();
+    } else {
+      cy.get('[data-testid="mobile_phone"]').type(phoneNumber);
+    }
 });
 
 Then(`Agent should be able to see lounge page`, () => {
   cy.wait('@loungePage');
-  cy.url().should('eq', `${Cypress.env('agency-agenttools').baseUrl}/lounge`);
-});
-
-Then(`Agent can see reactivasi account page`, () => {
-  cy.url().should('eq', `${Cypress.env('agency-agenttools').baseUrl}/reaktivasi?source=login`);
-});
-
-Then(`Agent can see pop up message for unregistered agent`, () => {
-  loginPage.popUpUnregisteredTitle().should('have.text', 'Akun anda belum terdaftar');
-  loginPage.popUpUnregisteredBody().should('have.text', 'Apakah anda ingin mendaftarkan akun anda?');
-  loginPage.popUpUnregisteredDaftarkanButton().should('be.visible');
-  loginPage.popUpUnregisteredCloseButton().should('be.visible');
-});
-
-Then(`Agent can see message must fill phone number`, () => {
-  loginPage.errorMessage().should('have.text', 'Masukkan Nomor Telepon');
-  loginPage.sendVerifiCodeButton().should('be.disabled');
+  cy.url().should('eq', `https://orientasi.sit.bravo.bfi.co.id/lounge`);
 });
 
 Then(`Agent can see {string} must input valid phone number`, (message) => {
-  loginPage.errorMessage().should('have.text', message);
-  loginPage.sendVerifiCodeButton().should('be.disabled');
+  cy.get('[data-testid="form-error-mobile_phone"]').should('have.text', message);
+  cy.get('#sendLoginCodeButton').should('be.disabled');
 });
 
-  When(`Agent login as {string} Agent`, (agentType) => {
+When(`Agent login as {string} Agent`, (phoneNumber) => {
     cy.wait('@homePage');
     cy.intercept('GET', '**/onboarding/v2/agent/profile').as('loungePage');
-    loginAgentUI(Cypress.env('agency-agenttools')[agentType].phone);
+    loginAgentUI(phoneNumber);
     cy.wait('@loungePage');
   });
 
